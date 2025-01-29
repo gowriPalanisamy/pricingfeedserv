61ced94ee1b4041d1a0f166f00af497b21e195ec
gowriPalanisamy <pgowri1915@gmail.com>
2025-01-29 16:00:42 +0530

    Update PricingDataService.java

--- src/main/java/com/tigeranalytics/pricingfeed/service/PricingDataService.java
+++ src/main/java/com/tigeranalytics/pricingfeed/service/PricingDataService.java
@@ -1,16 +1,16 @@
 package com.tigeranalytics.pricingfeed.service;
 
-import com.tigeranalytics.pricingfeed.dataobj.PricingDataDO;
 import com.tigeranalytics.pricingfeed.model.PricingData;
 import com.tigeranalytics.pricingfeed.model.Response;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public interface PricingDataService {
     List<PricingData> findByStoreIdAndSku(Long storeId, String sku);
     PricingData updatePricingData(Long id, PricingData updatedData);
-    Response validateAndSaveFile(MultipartFile pricingFeedCSVFile, String extension);
+    CompletableFuture<Response> validateAndSaveFile(MultipartFile pricingFeedCSVFile, String extension);
 }
