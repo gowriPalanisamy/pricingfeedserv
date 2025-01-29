7e2f69df8f6040058c5ed44ae481406386c060db
gowriPalanisamy <pgowri1915@gmail.com>
2025-01-29 15:51:11 +0530

    Update PricingFeedApplication.java

--- src/main/java/com/tigeranalytics/pricingfeed/PricingFeedApplication.java
+++ src/main/java/com/tigeranalytics/pricingfeed/PricingFeedApplication.java
@@ -2,9 +2,11 @@ package com.tigeranalytics.pricingfeed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(scanBasePackages = {"com.tigeranalytics.pricingfeed",
 		"com.tigeranalytics.pricingfeed.repository"})
+@EnableAsync
 public class PricingFeedApplication {
 
 	public static void main(String[] args) {
